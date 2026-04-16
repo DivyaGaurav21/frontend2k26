375d585109ba69f5121e8bfdce6dffc485d7f766
DivyaGaurav21 <divyagaurav1602045@gmail.com>
2026-04-16 05:35:08 -0700

    add debaunce example

---
+++ frontend-30/src/day-4/Debaunce.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+
+const API_URL = "https://dummyjson.com/products/search";
+
+const Debaunce = () => {
+  const [search, setSearch] = useState("");
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [cache, setCache] = useState({});
+
+  console.log(cache);
+
+  async function searchProduct(query) {
+    if (cache[query]) {
+      setProduct(cache[query]);
+      return;
+    }
+    try {
+      setLoading(true);
+      let jsonData = await fetch(`${API_URL}?q=${query}`);
+      let data = await jsonData.json();
+      setProduct(data.products);
+      setCache((prev) => ({ ...prev, [query]: data.products }));
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      if (search) {
+        searchProduct(search);
+      }
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [search]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="search.."
+      />
+      {loading && <p>Loading...</p>}
+      <div className="items">
+        {product.map((item) => (
+          <div className="item" key={item.id}>
+            <img src={item.thumbnail} width={100} alt={item.title} />
+            <p>{item.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Debaunce;
